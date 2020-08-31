dfe676a0459c637ab8f4183a64e93881dc448c2b
Michael Max <60975328+mikedanzon@users.noreply.github.com>
2020-08-31 22:54:00 +0700

    asd

---
+++ Front End/day6/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>tester</h1>
+    </div>
+  );
+}
