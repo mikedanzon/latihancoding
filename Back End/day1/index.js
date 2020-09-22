202140788367be7540ef9f4785cc6b8e4e26005f
Michael Max <60975328+mikedanzon@users.noreply.github.com>
2020-09-22 11:57:22 +0700

    asd

---
+++ Back End/day1/index.js
@@ -0,0 +1,4 @@
+var { nama, namalengkap } = require("./satu")
+
+console.log(nama)
+console.log(namalengkap("hahaha"))
---
+++ Back End/day1/satu.js
@@ -0,0 +1,8 @@
+var nama = "maxxx"
+
+module.exports = {
+    nama,
+    namalengkap(namabelakang) {
+        return this.nama + " " + namabelakang;
+    }
+} // export default nama
